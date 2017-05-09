35dbd6d85d9096841364081df27cd0a24ec8629b
t.goto <t.goto@chatbox-inc.com>
2017-05-09 10:34:25 +0900

    BasicTaskList

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,36 @@
 |
 */
 
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+	$tasks = Task::orderBy('created_at', 'asc')->get();
+	return view('task', [
+		'tasks' => $tasks
+	]);
+});
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:10',
+	]);
+	if ($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+	return redirect('/');
+});
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+	$task->delete();
+	return redirect('/');
 });
